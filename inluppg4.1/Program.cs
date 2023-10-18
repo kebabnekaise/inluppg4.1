d40e0cf3f50d0149aa806484990333e0983974c3
emsi06001 <emsi06001@E-PW0234N3>
2023-10-18 12:19:23 +0200

    Add project files.

---
+++ inluppg4.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace inluppg4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i  = 10; i <= 30; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for (int j = 200; j >= 180; j--)
+            {
+                Console.WriteLine(j);
+            }
+            for (int k = 1000; k <= 1400; k+=50)
+            {
+                Console.WriteLine(k);
+            }
+        }
+    }
+}
